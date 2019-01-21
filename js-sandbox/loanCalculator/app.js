e100a2bb1f5d760734d9ac0db180b590a548206b
Neil Berg <neil.berg14@gmail.com>
2019-01-21 13:25:31 -0800

    initial commit

---
+++ js-sandbox/loanCalculator/app.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('#loan-form');
+
+
+function calculateResults(e) {
+  e.preventDefault();
+  console.log('Calculating');
+  // UI variables
+  const amount = document.querySelector('#amount');
+  const interest = document.querySelector('#interest');
+  const years = document.querySelector('#years');
+  const monthlyPayment = document.querySelector('#monthly-payment');
+  const totalPayment = document.querySelector('#total-payment');
+  const totalInterest = document.querySelector('#total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value / 100 / 12);
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    showError('Please check your numbers')
+  }
+}
+
+function showError(error) {
+  
+  const errorDiv = document.createElement('div');
+
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  errorDiv.className = 'alert alert-danger';
+  errorDiv.appendChild(document.createTextNode(error));
+
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(() => document.querySelector('.alert').remove(), 3000)
+}
+
+form.addEventListener('submit', calculateResults);
