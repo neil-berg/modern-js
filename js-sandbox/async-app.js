324f9c69a75b931049454abc57f1680f609d95f5
Neil Berg <neil.berg14@gmail.com>
2019-02-01 14:08:48 -0800

    xhr with multiple JSON objects

--- js-sandbox/async-app.js
+++ js-sandbox/async-app.js
@@ -1,29 +1,87 @@
-const button = document.querySelector('#button');
+// ** XHR ** 
 
-function loadData(e) {  
-  // Create an XHR object
-  const xhr = new XMLHttpRequest();
-  
-  // Open
-  xhr.open('GET', 'data.txt', true);
-  
-  xhr.onprogress = function() {
-    console.log(xhr.readyState);
+// const button = document.querySelector('#button');
+
+// function loadData(e) {  
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'data.txt', true);
+//   xhr.onprogress = function() {
+//     console.log(xhr.readyState);
     
-  }
+//   }
+//   xhr.onload = function() {    
+//     if (this.status === 200) {
+//       document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+//     }
+//   }
+//   xhr.onerror = function() {
+//     console.log('Request error...'); 
+//   }
+//   xhr.send();
+// }
 
-  xhr.onload = function() {    
+// button.addEventListener('click', loadData);
+
+// XHR with JSON
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+// Single customer
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'customer.json', true);
+  xhr.onload = function() {
     if (this.status === 200) {
-      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+      const customer = JSON.parse(this.responseText);
+      const output = `
+      <ul>
+        <li>ID: ${customer.id}</li>
+        <li>Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+      </ul>
+      `
+    document.getElementById('customer').innerHTML = output;
     }
   }
-
-  xhr.onerror = function() {
-    console.log('Request error...');
-    
-  }
-
-  xhr.send();
+  xhr.send()
 }
 
-button.addEventListener('click', loadData);
+// Multiple customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'customers.json', true);
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);      
+      // let output = ''
+      // customers.forEach(customer => {
+      //   output += `
+      //   <ul>
+      //     <li>ID: ${customer.id}</li>
+      //     <li>Name: ${customer.name}</li>
+      //     <li>Company: ${customer.company}</li>
+      //     <li>Phone: ${customer.phone}</li>
+      //   </ul>
+      //   `
+      // })
+      const output = customers.map(function(customer) {
+        return`
+          <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>`
+      }).join('');
+
+    document.getElementById('customers').innerHTML = output;
+    }
+  }
+  xhr.send()
+}
+
+
+
+button1.addEventListener('click', loadCustomer);
+button2.addEventListener('click', loadCustomers);
