5c4988513e269d5d7a1a1b34ffe0b0c532ae7586
Neil Berg <neil.berg14@gmail.com>
2019-02-01 21:26:49 -0800

    initial commit

---
+++ js-sandbox/easyHTTP/easyhttp.js
@@ -0,0 +1,49 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest();
+}
+
+// Make an HTTP GET request
+easyHTTP.prototype.get = function(url, callback) {
+  this.http.open('GET', url, true);
+  this.http.onload = () => {
+    if (this.http.status === 200) {
+      callback(null, this.http.responseText);
+    } else {
+      callback(`Error: ${this.http.status}`);
+    }
+  }
+  this.http.send();
+}
+
+// Make an HTTP POST request
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+  this.http.onload = () => {
+    callback(null, this.http.responseText);
+  }
+  this.http.send(JSON.stringify(data));
+}
+
+// Make an HTTP PUT request
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+  this.http.onload = () => {
+    callback(null, this.http.responseText);
+  }
+  this.http.send(JSON.stringify(data));
+}
+
+// Make an HTTP DELETE request
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open('DELETE', url, true);
+  this.http.onload = () => {
+    if (this.http.status === 200) {
+      callback(null, 'Post deleted');
+    } else {
+      callback(`Error: ${this.http.status}`);
+    }
+  }
+  this.http.send();
+}
