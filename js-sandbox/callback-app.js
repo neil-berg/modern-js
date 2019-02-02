50587d72cf0522866854334e0953440c0885b19a
Neil Berg <neil.berg14@gmail.com>
2019-02-01 21:38:38 -0800

    using promises

--- js-sandbox/callback-app.js
+++ js-sandbox/callback-app.js
@@ -23,12 +23,36 @@ const posts = [
 // getPosts();
 
 // Using callbacks! 
+// function createPost(post, callback) {
+//   setTimeout(function() {
+//     posts.push(post)
+//     callback();
+//   }, 2000);
+// }
 
-function createPost(post, callback) {
-  setTimeout(function() {
-    posts.push(post)
-    callback();
-  }, 2000);
+// function getPosts() {
+//   setTimeout(function() {
+//     let output = '';
+//     posts.forEach(function(post) {
+//       output += `<li>${post.title}</li>`
+//     })
+//     document.body.innerHTML = output;
+//   }, 1000)
+// }
+
+// Using ES6 promises
+function createPost(post) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      posts.push(post)
+      let error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject('Error: something went wrong');
+      }
+    }, 2000);
+  });
 }
 
 function getPosts() {
@@ -41,4 +65,8 @@ function getPosts() {
   }, 1000)
 }
 
-createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+const newPost = {title: 'Post Three', body: 'This is post three'};
+
+createPost(newPost)
+  .then(getPosts)
+  .catch((err) => console.log(err));
